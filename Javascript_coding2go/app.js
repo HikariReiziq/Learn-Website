fe9552306e12d3b878d23da5bfc3d557fc623f11
HikariReiziq <reiziqzip@gmail.com>
2025-08-29 13:43:25 +0700

    episode 1

---
+++ Javascript_coding2go/app.js
@@ -0,0 +1,4 @@
+alert("bruh");
+console.log('Ini adalah tipe data String (teks).');
+console.log(100); // Ini adalah tipe data Number (angka).
+console.log(true); // Ini adalah tipe data Boolean (benar atau salah).
