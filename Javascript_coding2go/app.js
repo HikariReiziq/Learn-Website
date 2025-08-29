4291587d32dac5c6d86365163f6b8c97336f0385
HikariReiziq <reiziqzip@gmail.com>
2025-08-29 14:27:50 +0700

    update

--- Javascript_coding2go/app.js
+++ Javascript_coding2go/app.js
@@ -2,3 +2,25 @@ alert("bruh");
 console.log('Ini adalah tipe data String (teks).');
 console.log(100); // Ini adalah tipe data Number (angka).
 console.log(true); // Ini adalah tipe data Boolean (benar atau salah).
+
+
+for (let i=1; i<=5; i++){
+    console.log("count:", i);
+}
+
+let score = 85;
+
+if (score >= 90){
+    console.log("Grade: A")
+} else if (score >= 80){
+    console.log ("Grade: B")
+} else {
+    console.log ("Grade: C or lower")
+};
+
+function greet(name){
+    console.log ("Hello, " + name + "!");
+}
+
+greet("hikari")  
+ 
