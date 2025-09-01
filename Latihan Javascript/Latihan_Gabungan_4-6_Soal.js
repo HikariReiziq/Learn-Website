6e0740bd3f2a0c654d5e6d2c8d50befe3b06e612
HikariReiziq <reiziqzip@gmail.com>
2025-09-01 22:01:03 +0700

    Fixing Latgab

--- Latihan Javascript/Latihan_Gabungan_4-6_Soal.js
+++ Latihan Javascript/Latihan_Gabungan_4-6_Soal.js
@@ -1,32 +1,48 @@
-function konversiNilai(nilaiAngka){
-    if(nilaiAngka >= 90){
+function konversiNilai(nilai){
+    if(nilai >= 90){
         nilai = "A (Sangat Baik)";
         return nilai;
-    } else if (nilaiAngka >= 80){
+    } else if (nilai >= 80){
         nilai = "B (Baik)";
         return nilai;
-    } else if (nilaiAngka >= 70){
+    } else if (nilai >= 70){
         nilai = "C (Cukup)";
         return nilai;
-    } else if (nilaiAngka >= 60){
+    } else if (nilai >= 60){
         nilai = "D (Perlu Perbaikan)";
         return nilai;
-    } else if (nilaiAngka < 60 ){
+    } else {
         nilai = "E (Gagal)"
+        return nilai;
     }
 }
 
 console.log("--- Laporan Hasil Konversi Nilai ---")
-const budi = konversiNilai(85);
-console.log("Siswa Budi (Nilai: 85) mendapatkan predikat:", (nilai));
+console.log("");
 
-const Ani = konversiNilai(92);
-console.log("Siswa Ani (Nilai: 92) mendapatkan predikat:", (nilai));
+const Nilai1 = 85;
+const Siswa1 = "budi";
+const Nilai2 = 92;
+const Siswa2 = "Ani";
+const Nilai3 = 58;
+const Siswa3 = "Candra";
+const Nilai4 = 75;
+const Siswa4 = "Dewi";
 
-const Candra = konversiNilai(58);
-console.log("Siswa Candra (Nilai: 58) mendapatkan predikat:", (nilai));
 
-const Dewi = konversiNilai(75);
-console.log("Siswa Dewi (Nilai: 75) mendapatkan predikat:", (nilai));
+let Hasil1 = konversiNilai(Nilai1);
+let Hasil2 = konversiNilai(Nilai2);
+let Hasil3 = konversiNilai(Nilai3);
+let Hasil4 = konversiNilai(Nilai4);
 
+let laporan = `
+Siswa "${Siswa1}" (Nilai: ${Nilai1}) mendapatkan predikat: ${Hasil1}
+Siswa "${Siswa2}" (Nilai: ${Nilai2}) mendapatkan predikat: ${Hasil2}
+Siswa "${Siswa3}" (Nilai: ${Nilai3}) mendapatkan predikat: ${Hasil3}
+Siswa "${Siswa4}" (Nilai: ${Nilai4}) mendapatkan predikat: ${Hasil4}
+`
+console.log(laporan);
+
+// console.log("Siswa" ${Siswa1} "(Nilai: "${nilai}") mendapatkan predikat:," ${Nilai1});
+console.log("");
 console.log("--- Proses Selesai ---")
