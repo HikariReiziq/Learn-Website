0e5fa6ac5e29861393dec0abac0176f91c05ed5c
M. Hikari Reiziq Rakhadinta <reiziqzip@gmail.com>
2025-09-08 17:24:19 +0700

    latihan bab 12

---
+++ Latihan Javascript/Latihan_Bab_12.js
@@ -0,0 +1,45 @@
+console.log("Latihan bab 12 | upgrade dari bab 11")
+
+const input = document.querySelector('.js-todo-input')
+const tombolInput = document.querySelector('.js-add-todo-button')
+const isi = document.querySelector('.js-todo-list-container')
+const kalender = document.querySelector('.js-due-date-input')
+
+const daftarIsi = []; //[IPA, BAHASA, MTK]
+
+console.log("isi dari daftar isi: ", daftarIsi);
+
+const renderTodoList = () => {
+    let isiHtml = ''; //Variabel untuk ditampilkan
+
+    // daftarIsi.forEach(function(task) {
+        
+    //     //isiHtml = ipa;
+    //     const temp = `<li>${task}`;
+    //     isiHtml = isiHtml + temp;
+    //     console.log(`forEach: ${isiHtml}`);
+    // })
+    daftarIsi.forEach((task) => {
+        const { ketikanTugas, ketikanKalender} = task ;
+        const temp =  `<li><div>${ketikanTugas} - ${ketikanKalender}<div>`;
+        isiHtml = isiHtml + temp;
+    });
+    isi.innerHTML = isiHtml;
+    console.log(`Muncul di HTML: ${isiHtml}`);
+};
+
+tombolInput.addEventListener('click', () => {
+    const ketikanTugas = input.value;
+    const ketikanKalender = kalender.value;
+    
+    daftarIsi.push({
+        ketikanTugas, 
+        ketikanKalender
+    });
+    
+    input.value= '';  
+    kalender.value= '';  
+    console.log(`mengeluarkan`, daftarIsi)
+    renderTodoList();
+    // console.log(`tombol berhasil, mengeluarkan ${daftarIsi}`);
+});
